5440974ae1350b550c442fde39288e3fb6bd5005
micit.fd.092013@gmail.com <micit.fd.092013@gmail.com@7897417a-eae0-d3ca-2c16-224ea08b0d19>
2014-01-07 18:41:30 +0000

    Creacion de aplicacion demostrativa en C#:   CSmwEIDTest

---
+++ trunk/aplicaciones_demostrativas/CS/CSmwEIDTest_VisualStudio-2010/CSmwEIDTest/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CSmwEIDTest
+{
+    public partial class Form1 : Form
+    {
+        PKCS11Controller m_Controller;
+        public Form1()
+        {
+            InitializeComponent();
+            m_Controller = new PKCS11Controller("beidpkcs11.dll");
+            m_Controller.SetIssuerCertificate("CA SINPE - PERSONA FISICA.cer");
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            List<string> readers = m_Controller.GetReadersList();
+            if (readers.Count > 0)
+            {
+                cbxCardReaders.Items.Add("[Selecccione un Card Reader]");
+                cbxCardReaders.SelectedIndex = 0;
+                cbxCardReaders.Items.AddRange(readers.ToArray());
+                statusStrip1.Items[0].Text = "Card Readers Cargados.";
+                groupBox1.Enabled = true;
+            }
+            else
+            {
+                statusStrip1.Items[0].Text = "No se detectaron Card Readers.";
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            btnLogin.Enabled = false;
+            groupBox2.Enabled = groupBox3.Enabled = false;
+            tbCardInfo.Text = string.Empty;
+            this.Cursor = Cursors.WaitCursor;
+            try
+            {
+                errorProvider1.Clear();
+                if (cbxCardReaders.SelectedIndex != 0)
+                {
+                    int slotIndex = (cbxCardReaders.SelectedIndex - 1);
+                    statusStrip1.Items[0].Text = "Slot Seleccionado[" + slotIndex + "]";
+                    if (tbTokenPassword.Text.Trim() == string.Empty)
+                    {
+                        string msg = "El Número de PIN es requerido";
+                        statusStrip1.Items[0].Text = msg;
+                        errorProvider1.SetError(tbTokenPassword, msg);
+                        return;
+                    }
+                    if (tbNumeroTarjeta.Text.Trim() == string.Empty)
+                    {
+                        string msg = "El Número de Tarjeta es requerido";
+                        statusStrip1.Items[0].Text = msg;
+                        errorProvider1.SetError(tbNumeroTarjeta, msg);
+                        return;
+                    }
+
+                    if (tbNumeroTarjeta.Text.Trim() == string.Empty)
+                    {
+                        string msg = "Debe proveer un Número de Tarjeta";
+                        statusStrip1.Items[0].Text = msg;
+                        errorProvider1.SetError(tbNumeroTarjeta, msg);
+                        return;
+                    }
+
+                    if (!m_Controller.Login(slotIndex, tbTokenPassword.Text.Trim()))
+                    {
+                        string msg = "PIN Incorrecto";
+                        statusStrip1.Items[0].Text = msg;
+                        errorProvider1.SetError(tbTokenPassword, msg);
+                        return;
+                    }
+                    else
+                    {
+                        string msg = "PIN Correcto";
+                        statusStrip1.Items[0].Text = msg;
+
+                        if (!m_Controller.NumeroTarjetaValida(slotIndex, tbNumeroTarjeta.Text.Trim()))
+                        {
+                            msg = "Debe proveer un Número de Tarjeta Válido";
+                            statusStrip1.Items[0].Text = msg;
+                            errorProvider1.SetError(tbNumeroTarjeta, msg);
+                            return;
+                        }
+                        groupBox2.Enabled = true;
+                        tbCardInfo.Text = m_Controller.GetTokenInfo(slotIndex);
+                    }
+
+                }
+                else
+                {
+                    string msg = "Debe Seleccionar un Card Reader";
+                    statusStrip1.Items[0].Text = msg;
+                    errorProvider1.SetError(cbxCardReaders, msg);
+                }
+            }
+            finally
+            {
+                btnLogin.Enabled = true;
+                this.Cursor = Cursors.Default;
+            }
+        }
+
+        private void cbxCardReaders_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            errorProvider1.Clear();
+            groupBox2.Enabled = groupBox3.Enabled = false;
+            statusStrip1.Items[0].Text = "Card Readers Cargados.";
+            tbTokenPassword.Text = tbNumeroTarjeta.Text = tbCardInfo.Text = tbTextFirmado.Text =  string.Empty;
+        }
+
+        private void btnFirmar_Click(object sender, EventArgs e)
+        {
+            errorProvider1.Clear();
+            btnFirmar.Enabled = false;
+            this.Cursor = Cursors.WaitCursor;
+            try
+            {
+                if (cbxCardReaders.SelectedIndex != 0)
+                {
+                    byte[] encryptedData = null;
+                    int slotIndex = (cbxCardReaders.SelectedIndex - 1);
+                    if (!m_Controller.Firmar(slotIndex, tbTokenPassword.Text.Trim(),
+                        System.Text.Encoding.UTF8.GetBytes(tbTextoAFirmar.Text), out encryptedData))
+                    {
+                        string msg = "PIN Incorrecto";
+                        statusStrip1.Items[0].Text = msg;
+                        errorProvider1.SetError(tbTokenPassword, msg);
+                        return;
+                    }
+                    else
+                    {
+                        tbTextFirmado.Text = System.Text.Encoding.UTF8.GetString(encryptedData);
+                        statusStrip1.Items[0].Text = "Texto Firmado Correctamente";
+                    }
+                }
+                else
+                {
+                    string msg = "Debe Seleccionar un Card Reader";
+                    statusStrip1.Items[0].Text = msg;
+                    errorProvider1.SetError(cbxCardReaders, msg);
+                }
+            }
+            finally
+            {
+                btnFirmar.Enabled = true;
+                this.Cursor = Cursors.Default;
+            }
+        }
+
+        private void btnAutenticar_Click(object sender, EventArgs e)
+        {
+            errorProvider1.Clear();
+            btnAutenticar.Enabled = false;
+            this.Cursor = Cursors.WaitCursor;
+            try
+            {
+                if (cbxCardReaders.SelectedIndex != 0)
+                {
+                    string error = string.Empty;
+                    int slotIndex = (cbxCardReaders.SelectedIndex - 1);
+                    if (!m_Controller.Autenticar(slotIndex, tbTokenPassword.Text.Trim(), out error))
+                    {
+                        statusStrip1.Items[0].Text = error;
+                        errorProvider1.SetError(btnAutenticar, error);
+                        return;
+                    }
+                    else
+                    {
+                        string msg = "Autenticación Exitosa";
+                        statusStrip1.Items[0].Text = msg;
+                        groupBox3.Enabled = true;
+                    }
+                }
+                else
+                {
+                    string msg = "Debe Seleccionar un Card Reader";
+                    statusStrip1.Items[0].Text = msg;
+                    errorProvider1.SetError(cbxCardReaders, msg);
+                }
+            }
+            finally
+            {
+                btnAutenticar.Enabled = true;
+                this.Cursor = Cursors.Default;
+            }
+        }
+
+        private void btnAutenticar_MouseDown(object sender, MouseEventArgs e)
+        {
+            statusStrip1.Items[0].Text = "Autenticando con servidor OCSP";
+        }
+
+        private void btnFirmar_MouseDown(object sender, MouseEventArgs e)
+        {
+            statusStrip1.Items[0].Text = "Firmando";
+        }
+
+        private void btnLogin_MouseDown(object sender, MouseEventArgs e)
+        {
+            statusStrip1.Items[0].Text = "Validando";
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                tbTokenPassword.PasswordChar = (char)0;
+            }
+            else
+            {
+                tbTokenPassword.PasswordChar = '*';
+            }
+
+        }
+    }
+}
---
+++ trunk/aplicaciones_demostrativas/CS/CSmwEIDTest_VisualStudio-2010/CSmwEIDTest/PKCS11Controller.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+
+using System.Runtime.InteropServices;
+
+using Net.Sf.Pkcs11;
+using Net.Sf.Pkcs11.Objects;
+using Net.Sf.Pkcs11.Wrapper;
+
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
+
+
+namespace CSmwEIDTest
+{
+    class PKCS11Controller
+    {
+        private String m_FileName;
+        private String m_AutenticacionLabel = "NOT_SET";
+        private String m_SignLabel = "NOT_SET";
+        private String m_IssuerCertificate = "NOT_SET";
+
+        private Module m_Module = null;
+        private Slot[] m_Slots = null;
+        private int m_CurrentIndex = -1;
+        public PKCS11Controller(String in_FileName)
+        {
+            m_FileName = in_FileName;
+        }
+        public List<String> GetReadersList(bool in_Refresh = false)
+        {
+            List<String> readers = new List<string>();
+            try
+            {
+                if (m_Module == null)
+                {
+                    m_Module = Module.GetInstance(m_FileName);
+                }
+
+                if (m_Slots == null)
+                {
+                    // GetSlotList.
+                    m_Slots = m_Module.GetSlotList(true);
+                }
+                else if (in_Refresh)
+                {
+                    //TODO verify
+                    //m_Slots = m_Module.GetSlotList(true);
+                }
+                foreach (Slot slot in m_Slots)
+                {
+                    readers.Add(slot.SlotInfo.SlotDescription);
+                    Console.WriteLine(slot.SlotInfo.SlotDescription);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return readers;
+        }
+
+        internal bool NumeroTarjetaValida(int in_SlotIndex, string in_NumeroTarjeta)
+        {
+            bool result = false;
+            if (m_Slots.Length > in_SlotIndex)
+            {
+                Slot slot = m_Slots[in_SlotIndex];
+                result = slot.Token.TokenInfo.SerialNumber == in_NumeroTarjeta;
+            }
+            return result;
+        }
+
+        internal bool Login(int in_SlotIndex, string in_PIN)
+        {
+            bool result = false;
+            try
+            {
+                if (m_Module == null)
+                {
+                    m_Module = Module.GetInstance(m_FileName);
+                }
+
+                if (m_Slots == null)
+                {
+                    // GetSlotList.
+                    m_Slots = m_Module.GetSlotList(true);
+                }
+                if (m_Slots.Length > in_SlotIndex)
+                {
+                    Slot slot = m_Slots[in_SlotIndex];
+                    Session session = slot.Token.OpenSession(false);
+                    m_CurrentIndex = in_SlotIndex;
+                    session.Login(UserType.USER, in_PIN);
+
+                    try
+                    {
+
+                        ObjectClassAttribute classAttribute = new ObjectClassAttribute(CKO.CERTIFICATE);
+                        //ByteArrayAttribute keyLabelAttribute = new ByteArrayAttribute(CKA.LABEL);
+                        //keyLabelAttribute.Value = System.Text.Encoding.UTF8.GetBytes(m_SignLabel);
+
+                        session.FindObjectsInit(new P11Attribute[] {
+                                 classAttribute
+                          //       keyLabelAttribute
+                                }
+                                );
+                        P11Object[] certificates = session.FindObjects(2) as P11Object[];
+                        if (certificates.Length == 2)
+                        {
+                            SetAutenticacionLabel(new string(((X509PublicKeyCertificate)certificates[0]).Label.Value));
+                            SetSignatureLabel(new string(((X509PublicKeyCertificate)certificates[1]).Label.Value));
+                        }
+                        
+                        session.FindObjectsFinal();
+
+                        ///////////////////
+                        result = true;
+                    }
+                    finally
+                    {
+                        // Log out.
+                        session.Logout();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return result;
+        }
+
+
+
+        internal bool Firmar(int in_SlotIndex, string in_PIN, byte[] in_Data, out byte[] out_encryptedData)
+        {
+            bool result = false;
+            out_encryptedData = null;
+            try
+            {
+                if (m_Module == null)
+                {
+                    m_Module = Module.GetInstance(m_FileName);
+                }
+
+                if (m_Slots == null)
+                {
+                    // GetSlotList.
+                    m_Slots = m_Module.GetSlotList(true);
+                }
+                if (m_Slots.Length > in_SlotIndex)
+                {
+                    Slot slot = m_Slots[in_SlotIndex];
+                    Session session = slot.Token.OpenSession(false);
+                    m_CurrentIndex = in_SlotIndex;
+                    session.Login(UserType.USER, in_PIN);
+
+                    try
+                    {
+                        ObjectClassAttribute classAttribute = new ObjectClassAttribute(CKO.PRIVATE_KEY);
+                        ByteArrayAttribute keyLabelAttribute = new ByteArrayAttribute(CKA.LABEL);
+                        keyLabelAttribute.Value = System.Text.Encoding.UTF8.GetBytes(m_SignLabel);
+
+                        session.FindObjectsInit(new P11Attribute[] {
+                                 classAttribute,
+                                 keyLabelAttribute
+                                }
+                                );
+                        P11Object[] privatekeys = session.FindObjects(1) as P11Object[];
+                        session.FindObjectsFinal();
+
+                        if (privatekeys.Length >= 1)
+                        {
+                            session.SignInit(new Mechanism(CKM.SHA1_RSA_PKCS), (PrivateKey)privatekeys[0]);
+                            out_encryptedData = session.Sign(in_Data);
+                        }
+                        result = true;
+                    }
+                    finally
+                    {
+                        // Log out.
+                        session.Logout();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return result;
+
+        }
+
+        internal void SetAutenticacionLabel(string in_AuthenticationLabel)
+        {
+            m_AutenticacionLabel = in_AuthenticationLabel;
+        }
+
+        internal void SetSignatureLabel(string in_SignLabel)
+        {
+            m_SignLabel = in_SignLabel;
+        }
+        internal void SetIssuerCertificate(string in_IssuerCertificate)
+        {
+            m_IssuerCertificate = in_IssuerCertificate;
+        }
+        
+        internal string GetTokenInfo(int in_SlotIndex)
+        {
+            if (m_Slots.Length > in_SlotIndex)
+            {
+                Slot slot = m_Slots[in_SlotIndex];
+                StringBuilder builder = new StringBuilder();
+                builder.Append("ManufacturerID: ");
+                builder.Append(slot.Token.TokenInfo.ManufacturerID);
+                builder.AppendLine();
+                builder.Append("Label: ");
+                builder.Append(slot.Token.TokenInfo.Label);
+                builder.AppendLine();
+                builder.Append("HardwareVersion: ");
+                builder.Append(slot.Token.TokenInfo.HardwareVersion.ToString());
+                builder.AppendLine();
+                builder.Append("FirmwareVersion: ");
+                builder.Append(slot.Token.TokenInfo.FirmwareVersion.ToString());
+                builder.AppendLine();
+                builder.Append("Model: ");
+                builder.Append(slot.Token.TokenInfo.Model);
+                return builder.ToString();
+            }
+            return String.Empty;
+        }
+
+        internal bool Autenticar(int in_SlotIndex, string in_PIN, out string out_Error)
+        {
+            bool result = false;
+            out_Error = "OK";
+
+            try
+            {
+                if (m_Module == null)
+                {
+                    m_Module = Module.GetInstance(m_FileName);
+                }
+
+                if (m_Slots == null)
+                {
+                    // GetSlotList.
+                    m_Slots = m_Module.GetSlotList(true);
+                }
+                if (m_Slots.Length > in_SlotIndex)
+                {
+                    Slot slot = m_Slots[in_SlotIndex];
+                    Session session = slot.Token.OpenSession(false);
+                    m_CurrentIndex = in_SlotIndex;
+                    session.Login(UserType.USER, in_PIN);
+
+                    try
+                    {
+                        ObjectClassAttribute certificateAttribute = new ObjectClassAttribute(CKO.CERTIFICATE);
+                        ByteArrayAttribute fileLabel = new ByteArrayAttribute(CKA.LABEL);
+                        fileLabel.Value = System.Text.Encoding.UTF8.GetBytes(m_AutenticacionLabel);
+
+                        session.FindObjectsInit(new P11Attribute[] {
+                                 certificateAttribute,
+                                 fileLabel
+                                }
+                                );
+                        P11Object[] foundObjects = session.FindObjects(1) as P11Object[];
+
+                        if (foundObjects.Length == 1)
+                        {
+                            X509PublicKeyCertificate cert = foundObjects[0] as X509PublicKeyCertificate;
+                            OcspClient oscpClient = new OcspClient(cert.Value.Encode());
+                            if (oscpClient.PublicKeyCertificate.IsValidNow)
+                            {
+                                CertificateStatus status = oscpClient.ConsultarEstadoDeCertificado(oscpClient.PublicKeyCertificate, oscpClient.LeerCertificado(m_IssuerCertificate));
+                                if (status == CertificateStatus.Good)
+                                {
+                                    result = true;
+                                }
+                                else if (status == CertificateStatus.Revoked)
+                                {
+                                    out_Error = "Certificado Revocado";
+                                }
+                                else
+                                {
+                                    out_Error = "Certificado Desconocido";
+                                }
+                            }
+                            else
+                            {
+                                out_Error = "Certificado Expirado";
+                            }
+                        }
+                        else
+                        {
+                            out_Error = "No se encontraron objetos en la tarjeta.";
+                        }
+
+                        session.FindObjectsFinal();
+                        
+                    }
+                    catch( System.Net.WebException wex) 
+                    {
+                        Console.WriteLine(wex.ToString());
+                        out_Error = wex.Message;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.ToString());
+                        out_Error = e.Message;
+                    }
+                    finally
+                    {
+                        // Log out.
+                        session.Logout();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return result;
+        }
+        
+    }
+}
