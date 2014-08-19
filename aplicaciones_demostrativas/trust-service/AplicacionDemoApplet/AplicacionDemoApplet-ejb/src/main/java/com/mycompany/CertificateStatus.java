12d76c68848e23410881353119034987f9d0fce2
micit.fd.092013 <micit.fd.092013@gmail.com>
2014-08-19 01:20:50 +0000

    Agregando aplicacion Java demostrativa para Trust-Service.

---
+++ aplicaciones_demostrativas/trust-service/AplicacionDemoApplet/AplicacionDemoApplet-ejb/src/main/java/com/mycompany/CertificateStatus.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany;
+
+/**
+ *
+ * @author jubarran
+ */
+public enum CertificateStatus {
+
+    Good, Revoked, UNKNOWN;
+}
